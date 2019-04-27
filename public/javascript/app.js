e0fe2cef228cbc4ab917cee5b4320b72fed0ce61
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.localdomain>
2019-04-27 19:02:34 -0400

    basic js code in place

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,42 +3,71 @@ document.addEventListener("DOMContentLoaded", function(){
 console.log("hello");
 var time;
 var timer;
-var clicker = false;
-var jbutton =  document.getElementById("jbutton");
-start();
+var countdownString = "TIME LEFT   "
 
+var q = 0;
+// var jbutton =  document.getElementById("jbutton");
+var startButton = document.getElementById("start");
+var tButton = document.getElementById("tr");
+var fbutton = document.getElementById("fl");
+var qTitle = document.getElementById("qT");
+var qMain = document.getElementById("qMain");
+
+welcome();
+
+function welcome(){
+    document.getElementById("triva").style.display = "none";
+    startButton.addEventListener("click", start);
+};
 function start(){
-    jbutton.style.display = "none";
-    clicker = false;
-    console.log("clicker" + clicker);
-//    console.log("start function");
+     startButton.style.display = "none";
+     
+    
     time = 10;
     timer = setInterval(stuff, 1000);
-}
+};
 
 function stuff(){
-    // console.log("stuff function");
     time -= 1;
     if(time <= 0){
         clearInterval(timer);
         loser();
     }
-    document.getElementById("p1").innerHTML = time;
-    // console.log("time"+ time);
-   
+    document.getElementById("triva").style.display = "block";
+    questionHolder(q);
+
+    document.getElementById("p1").innerHTML = countdownString + time;
 };
 
 function loser(){
+    q++;
+   console.log("u loose");
+   //hide buttons and show losing screen
+};
+function winner(){
+    q++;
+    console.log("u win");
+    //hide buttons and show winner screen
+    //add score
+}
 
-   
-    clicker = true;
-    console.log("clicker " + clicker);
-    if(clicker === true){
-        jbutton.style.display = "block";
-       jbutton.addEventListener("click", start);
-       
-    } 
-    
+function questionHolder(q){
+    switch (q) {
+        case 0:
+         qTitle.style.display = "block";
+         qMain.style.display ="block";
+         qTitle.innerText = "Question 1";
+         qMain.innerText = "Kermit the frog said it's not easy being blue";
+         tButton.addEventListener("click",loser);
+         fbutton.addEventListener("click",winner);
+        break;
+        case 1:
+          console.log("2");
+        break;
+        case 2:
+          console.log("3");
+        break;  
+    }
 };
 
 
